88c977d1f83c883b1fbc957d94f6b39bee7d351d
jorge-vnarvaez <jorge.vnarvaez@gmail.com>
2023-07-28 10:51:48 -0400

    fix: footer images size

--- components/nav/footer.vue
+++ components/nav/footer.vue
@@ -4,9 +4,9 @@
         <div class="w-full py-5">
           <div class="font-sans text-bold text-lg pb-3">Organización</div>
           <div class="grid grid-cols-12 gap-x-2">
-            <q-img class="col-span-1 h-40" fit="contain" src="/logo_fia.png"></q-img>
-            <q-img class="col-span-1  h-40" fit="contain" src="/logo_udec.png"></q-img>
-            <q-img class="col-span-1  h-40" fit="contain" src="/logo iit.png"></q-img> 
+            <q-img class="col-span-4 lg:col-span-1 h-40" fit="contain" src="/logo_fia.png"></q-img>
+            <q-img class="col-span-4 lg:col-span-1 h-40" fit="contain" src="/logo_udec.png"></q-img>
+            <q-img class="col-span-4 lg:col-span-1 h-40" fit="contain" src="/logo iit.png"></q-img> 
           </div>
         </div>
     </q-toolbar>
